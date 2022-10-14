c01a8234e8a35630882cee0ec8656e842c1a7e13
AHDiniz <alanhdiniz@gmail.com>
2022-10-13 23:08:10 -0300

    Implementing the player agent

--- Assets/Scripts/MachineLearning/PlayerAgent.cs
+++ Assets/Scripts/MachineLearning/PlayerAgent.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using JustAnotherShmup.Player;
+using JustAnotherShmup.Stats;
+using JustAnotherShmup.Management;
 using JustAnotherShmup.UserInterface;
 
 namespace JustAnotherShmup.MachineLearning
@@ -14,21 +16,31 @@ namespace JustAnotherShmup.MachineLearning
         private bool _shootBullets;
         private bool _shootMissile;
 
+        private HealthPoints _hp;
+        private MissileCountDisplay _missileCount;
+        private Scoring _score;
+
         Vector2 IPlayerInputs.Movement { get => _movement; }
         bool IPlayerInputs.ShootBullets { get => _shootBullets; }
         bool IPlayerInputs.ShootMissile { get => _shootMissile; }
 
         public override void OnEpisodeBegin()
         {
-            
-        }
+            _hp = GetComponent<HealthPoints>();
 
-        public override void OnActionReceived(float[] vectorActions)
-        {
-            
+            GameObject gameController = GameObject.FindWithTag("GameController");
+            _missileCount = gameController.GetComponent<MissileCountDisplay>();
+            _score = gameController.GetComponent<Scoring>();
         }
 
         public override void CollectObservations(VectorSensor sensor)
+        {
+            sensor.AddObservation(transform.position);
+            sensor.AddObservation(_hp.CurrentHP);
+            sensor.AddObservation(_missileCount.CurrentAmmo);
+        }
+
+        public override void OnActionReceived(float[] vectorActions)
         {
             
         }
